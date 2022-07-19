ad012c54a2f9a6a89e7c8d8f0f97d032dea9fe25
hanhnd <hanhnd156@gmail.com>
2022-07-19 16:06:57 +0700

    add rcv

---
+++ StudentManager/app/src/main/java/me/hanhngo/studentmanager/MainActivity.java
@@ -0,0 +1,63 @@
+package me.hanhngo.studentmanager;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import me.hanhngo.studentmanager.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding = null;
+
+    SQLiteDatabase db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Open DB
+        String path = getFilesDir() + "/mydb";
+        try {
+            db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+//        createTable();
+
+        ArrayList<StudentModel> studentModelArrayList = DBUtil.selectAll(db);
+        StudentAdapter adapter = new StudentAdapter(studentModelArrayList);
+        binding.studentRcv.setAdapter(adapter);
+    }
+
+    public void createTable() {
+        db.beginTransaction();
+        try {
+            db.execSQL("create table tbStudent(" +
+                    "student_id integer PRIMARY KEY autoincrement," +
+                    "full_name text," +
+                    "email text," +
+                    "dob text)");
+
+            db.execSQL("insert into tbStudent(full_name, email, dob) values('ngo', 'ngo@gmail.com', '12/12/2021')");
+            db.execSQL("insert into tbStudent(full_name, email, dob) values('ngo1', 'ngo@gmail.com', '13/12/2021')");
+            db.execSQL("insert into tbStudent(full_name, email, dob) values('ngo2', 'ngo@gmail.com', '14/12/2021')");
+            db.setTransactionSuccessful();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        db.close();
+        super.onDestroy();
+    }
+}
